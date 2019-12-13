7106a9becd3d38c7dc3dcb4a9ab616370c1b67b9
Jennifer Lipps <jjlipps@gmail.com>
2019-12-12 20:32:34 -0600

    authentication is working, can login with google account

--- src/App/App.js
+++ src/App/App.js
@@ -3,8 +3,11 @@ import React from 'react';
 import Home from '../Components/Home/Home';
 import Navbar from '../Components/MyNavbar/MyNavbar';
 import Auth from '../Components/Auth/Auth';
+import fbConnection from '../Helpers/fbConnection';
 
 import './App.scss';
+
+fbConnection();
 
 class App extends React.Component {
   render() {
