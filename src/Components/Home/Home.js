54166cd4622cc173014a77e02698a2c786a25b5e
Bob <bob.e.baxter@gmail.com>
2020-01-04 21:22:31 -0600

    wip working on search in home.js

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,25 +1,59 @@
 import React from 'react';
 
+import Fuse from 'fuse.js';
 import ProductsData from '../../Helpers/Data/ProductsData';
 
 import ProductCard from '../ProductCard/ProductCard';
+
 
 import './Home.scss';
 
 class Home extends React.Component {
   state = {
-    latestProducts: [],
+    allProducts: [],
+    productsResults: [],
+    query: '',
+  }
+
+  searchOptions = {
+    shouldSort: true,
+    threshold: 0.6,
+    maxPatternLength: 32,
+    minMatchCharLength: 1,
+    keys: [
+      'clubName',
+      'productName',
+    ],
+  };
+
+  fuse = new Fuse(this.state.allProducts, this.searchOptions);
+
+  // useEffect(() => {
+  //   setAllProducts(this.state.query
+  //     ? this.fuse.search(this.state.query) : this.state.allProducts);
+  // }, [query]);
+
+  updateQuery = (queryValue) => {
+    this.setState({ query: queryValue });
+    const newProductsResults = queryValue
+      ? this.fuse.search(queryValue)
+      : this.state.allProducts;
+    this.setState({ productsResults: newProductsResults });
   }
 
   componentDidMount() {
     ProductsData.getLatestProducts()
-      .then((res) => this.setState({ latestProducts: res }))
+      .then((res) => this.setState({ productsResults: res }))
+      .catch((err) => console.error(err));
+    ProductsData.getAllProducts()
+      .then((res) => this.setState({ allProducts: res }))
       .catch((err) => console.error(err));
   }
 
   render() {
-    const { latestProducts } = this.state;
-    const printLatestProducts = latestProducts.map((product) => {
+    console.error(this.fuse.search('h'));
+    const { productsResults } = this.state;
+    const printLatestProducts = productsResults.map((product) => {
       const uniqueKey = `${product.productName.charAt(0)}${product.clubProductId}`;
       return (
         <ProductCard
@@ -31,6 +65,11 @@ class Home extends React.Component {
     return (
       <div className="Home">
         <div className="home-sidebar">
+          <input
+            type='search'
+            value={this.state.query}
+            onChange={(event) => this.updateQuery(event.currentTarget.value)}
+          />
           <h4>Product Categories</h4>
           <ul>
             <li>Tickets (20)</li>
@@ -41,7 +80,7 @@ class Home extends React.Component {
         <div className="home-main">
           <h1>Products</h1>
           <div className="home-main-productWindow">
-            {latestProducts ? printLatestProducts : ''}
+            {productsResults ? printLatestProducts : ''}
           </div>
         </div>
       </div>
--- src/Components/MyNavbar/MyNavbar.js
+++ src/Components/MyNavbar/MyNavbar.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { NavLink as RRNavLink } from 'react-router-dom';
 import {
   Collapse,
@@ -9,40 +9,17 @@ import {
   NavItem,
   NavLink,
 } from 'reactstrap';
-import Fuse from 'fuse.js';
 
 import './MyNavbar.scss';
 
 const MyNavbar = (props) => {
   const [isOpen, setIsOpen] = useState(false);
-  const [allProducts, setAllProducts] = useState('');
-  const [query, setQuery] = useState('');
-  const searchOptions = {
-    shouldSort: true,
-    threshold: 0.6,
-    maxPatternLength: 32,
-    minMatchCharLength: 1,
-    keys: [
-      'clubName',
-      'productName',
-    ],
-  };
-  const fuse = new Fuse(props.allProducts, searchOptions);
 
   const toggle = () => setIsOpen(!isOpen);
-
-  useEffect(() => {
-    setAllProducts(query ? fuse.search(query) : props.allProducts);
-  }, [query, allProducts, props.allProducts, fuse]);
 
   return (
     <Navbar color="dark" dark expand="md">
       <NavbarBrand href="/">Fight & Feast</NavbarBrand>
-      <input
-        type='search'
-        value={query}
-        onChange={(event) => setQuery(event.currentTarget.value)}
-      />
       <NavbarToggler onClick={toggle} />
       <Collapse isOpen={isOpen} navbar>
         <Nav className="ml-auto" navbar>
